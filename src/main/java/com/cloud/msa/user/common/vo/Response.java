b51ed8564faada74ef3aabda1fe5a89068d659fa
aibees <aibees1129@naver.com>
2022-05-29 21:07:20 +0900

    response vo 객체 생성

---
+++ src/main/java/com/cloud/msa/user/common/vo/Response.java
@@ -0,0 +1,15 @@
+package com.cloud.msa.user.common.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Setter
+@Getter
+@Builder
+public class Response {
+
+    private String message;
+    private Object data;
+}
